0575b2fc3f0b9a4d9692d931cd6655a874895861
WhiteSouls20 <mat.orostica@duocuc.cl>
2025-05-06 19:29:46 -0400

    tres de cuatro

---
+++ Project-Dealer-FrontEnd-Manuel/src/app/core/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private LOGIN_URL = 'http://127.0.0.1:8000/token/';
+  private tokenKey = 'authToken';
+
+  constructor(private httpClient: HttpClient, private router: Router) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.httpClient.post<any>(this.LOGIN_URL, { username, password }).pipe(
+      tap(response => {
+        if (response.access) {
+          console.log(response.access);
+          this.setToken(response.access);
+        }
+      })
+    );
+  }
+
+  private setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  private getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  isAuthenticated(): boolean {
+    const token = this.getToken();
+    if (!token) {
+      return false;
+    }
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const exp = payload.exp * 1000;
+    return Date.now() < exp;
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    this.router.navigate(['/login']);
+  }
+}
